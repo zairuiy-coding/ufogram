bf18de9583463a4376640fbd6ba0e537d7e01932
Zairui Yang <zairuiy@seas.upenn.edu>
2023-10-04 18:01:55 -0400

    setup login

--- ufogram/src/views/Login.js
+++ ufogram/src/views/Login.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function handleLogin() {
+function Login() {
     // if account does not exist, retry or jump to registration page
     // if password incorrect, retry, block if wrong for 3 times
     // if password correct, jump to Main_view
-}
 
-function handleSignup() {
-    // ...
-}
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-export default function Login() {
+    const handleLogin = () => {
+
+    };
+
+    const handleSignup = () => {
+
+    }
+
     return (
         <div>
             <h1>UFOgram</h1>
             <div>
-                <input type="text" name="Username" />
-                <input type="text" name="Password" />
-                <button title="Log in" onClick = { handleLogin }/>
-                <button title="Sign up" onClick = { handleSignup }/>
+                <label htmlFor='Username'>Username:</label>
+                <input type="text" name="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <br />
+                <label htmlFor='Password'>Password:</label>
+                <input type="password" name="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <br />
+                <button title="Log in" onClick={handleLogin} />Login
+                <button title="Sign up" onClick={handleSignup} />Signup
             </div>
         </div>
     );
 }
+
+export default Login;
+
+
