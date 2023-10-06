77cb96d91bd1c7c2e777a7ce37b7493967312bc9
Yuan Zhang <yz5892@seas.upenn.edu>
2023-10-06 15:31:37 -0400

    Create Post.js

---
+++ ufogram/src/views/Post.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default async function create(user, image, caption) {
+    try {
+        const response = await axios.post('http://localhost:3000/posts', {
+        image: image,
+        caption: caption,
+    });
+    if (response.status === 201) {
+        return response.data;
+    } else {
+        return 'Error: Post creation failed';
+    }
+    } catch(e) {
+        console.error('Error:', e.message);
+        throw e;
+    }
+}
