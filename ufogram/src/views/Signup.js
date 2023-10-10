c079fccfeb95ac4708876cf1ffaed70b096142b4
Zairui Yang <zairuiy@seas.upenn.edu>
2023-10-10 17:28:15 -0400

    complete signup and login

--- ufogram/src/views/Signup.js
+++ ufogram/src/views/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import register from '../api/user';
+import register from '../api/registration';
+import getUsers from '../api/user';
 
 function Signup() {
     // if account does not exist, retry or jump to registration page
@@ -10,10 +11,23 @@ function Signup() {
     let [username, setUsername] = useState('');
     let [password, setPassword] = useState('');
 
-    const handleSignup = () => {
+    const handleSignup = async () => {
         console.log('User info', username, password);
-        const status = register(username, password);
-        console.log('Status', status);
+        const response = await getUsers();
+        for (let i = 0; i < response.data.length; i++) {
+                    
+            if (response.data[i].username === username && response.data[i].password === password) {
+                return;
+            }
+        }
+        try {
+            const status = await register(username, password);
+            console.log('Status', status);
+            document.getElementById('signup').innerHTML = `Welcome, ${username}`;
+        } catch (err) {
+
+        }
+        
     }
 
     const navigate = useNavigate();
@@ -33,14 +47,14 @@ function Signup() {
     return (
         <div style={{display: "flex", justifyContent: "space-evenly"}}>
             <div style={{display: "flex", justifyContent: "center", position: "fixed", width: "100%", background: "#8769b6"}}>
-                <h1>UFOgram</h1>
+                <h1>UFOgram Signup</h1>
             </div>
             <div style={{display: "flex", width: "12%", marginTop: "100px", flexDirection: "column"}}>
                 <label htmlFor='Username'>Username: </label>
-                <input type="text" name="Username" value={username} onChange={handleUsernameChange}/>
+                <input data-testID='uname' type="text" name="Username" value={username} onChange={handleUsernameChange}/>
                 <label htmlFor='Password'>Password: </label>
                 <input type="password" name="Password" value={password} onChange={handlePasswordChange}/>
-                <button title="Sign up" onClick={handleSignup}>Signup</button>
+                <button id='sign_up' title="Sign up" onClick={handleSignup}>Signup</button>
                 <button title="Log in" onClick={handleLogin} style={{color: "#808080"}}>Login</button>
             </div>
         </div>
