57601eb6274895390f31ddfa42c854f025958414
Zairui Yang <zairuiy@upenn.edu>
2023-11-05 23:03:17 -0500

    all backend tests for Users completed

--- ufogram_backend/server.js
+++ ufogram_backend/server.js
@@ -133,8 +133,12 @@ webapp.delete('/Users/:userId', async (req, res) => {
       console.log('result === undefined');
       return;
     }
-    res.status(200).json({ data: result });
-    console.log('result: ', result);
+    if (result.deletedCount === 0) {
+      res.status(404).json({ error: 'no user deleted' });
+    } else {
+      res.status(200).json({ data: result });
+      console.log('result: ', result);
+    }
   } catch (err) {
     res.status(404).json({ error: err.message });
     console.log('err: ', err);
@@ -304,7 +308,11 @@ webapp.delete('/Posts/:postId', async (req, res) => {
       res.status(404).json({ error: 'bad post ID' });
       return;
     }
-    res.status(200).json({ data: result });
+    if (result.deletedCount === 0) {
+      res.status(404).json({ error: 'no post deleted' });
+    } else {
+      res.status(200).json({ data: result });
+    }
   } catch (err) {
     res.status(404).json({ error: err.message });
   }
