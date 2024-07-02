b2a7e801ab592e5f0d2365a7717dcebe900770db
Zairui Yang <zairuiy@upenn.edu>
2024-07-02 18:03:42 -0400

    comment out debug log

--- ufogram_backend/s3Operations.js
+++ ufogram_backend/s3Operations.js
@@ -2,10 +2,10 @@ const { Upload } = require('@aws-sdk/lib-storage');
 const { S3 } = require('@aws-sdk/client-s3');
 require('dotenv').config();
 
-console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
-console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
-console.log('AWS_REGION:', process.env.AWS_REGION);
-console.log('AWS_S3_BUCKET_NAME:', process.env.AWS_S3_BUCKET_NAME);
+// console.log('AWS_ACCESS_KEY_ID:', process.env.AWS_ACCESS_KEY_ID);
+// console.log('AWS_SECRET_ACCESS_KEY:', process.env.AWS_SECRET_ACCESS_KEY);
+// console.log('AWS_REGION:', process.env.AWS_REGION);
+// console.log('AWS_S3_BUCKET_NAME:', process.env.AWS_S3_BUCKET_NAME);
 
 const BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
 
