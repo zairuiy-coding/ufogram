7c63e2bb35b7464e19404f71b5db17fdb0251cf3
Zairui Yang <zairuiy@upenn.edu>
2024-07-02 16:22:30 -0400

    Add eslint-plugin-import and update ESLint configuration

--- ufogram/.eslintrc.js
+++ ufogram/.eslintrc.js
@@ -1,42 +1,47 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: 'airbnb',
-  overrides: [
-    {
-      env: {
-        node: true,
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+      jest: true,
+    },
+    extends: ['airbnb', 'airbnb/hooks'],
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: [
+          '.eslintrc.{js,cjs}',
+        ],
+        parserOptions: {
+          sourceType: 'script',
+        },
       },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
       },
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: { // <-- Added ecmaFeatures to understand JSX
-      jsx: true,
+    plugins: ['import'], // Ensure 'import' plugin is included
+    rules: {
+      'import/no-extraneous-dependencies': ['error', {
+        devDependencies: ['**/*.test.js', '**/*.spec.js'],
+        optionalDependencies: false,
+        peerDependencies: false,
+        packageDir: './',
+      }],
     },
-  },
-  rules: {
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: ['**/*.test.js', '**/*.spec.js'], optionalDependencies: false, peerDependencies: false, packageDir: './',
-    }],
-  },
-  settings: { // <-- Added settings to auto-detect React version
-    react: {
-      version: 'detect',
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
-  },
-  globals: {
-    PropTypes: 'readonly', // Define PropTypes as a global variable
-  },
-};
+    globals: {
+      PropTypes: 'readonly',
+    },
+  };
+  
